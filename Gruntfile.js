e09238ab933a898deb86f87e35202f54c19e08ec
airyogurt <sivuoliviu@yahoo.com>
2015-04-04 22:37:53 +0300

    added css helper

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
           optimization: 2
         },
         files: {
-          "css/main.css": "less/test.less" // destination file and source file
+          "public/media/css/main.css": "public/media/less/test.less" // destination file and source file
         }
       }
     },
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,8 +5,9 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $test = new Application_Model_mapper_TestMapper();
-        $this->view->test = $test->getTestData();
+        /*$test = new Application_Model_mapper_TestMapper();
+        $this->view->test = $test->getTestData();*/
+
     }
 
 }

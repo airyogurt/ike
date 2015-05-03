8946ef59bdb01d886e69b06a417746b640c523ee
airyogurt <sivuoliviu@yahoo.com>
2015-05-03 16:43:32 +0300

    changed view format

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,8 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+  protected function _initViewSuffix()
+  {
+      Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer')->setViewSuffix('html');
+  }
 }
-
